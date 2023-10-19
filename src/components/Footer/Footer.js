f99e7625066b8b6b49eebbdf5733bf43e6cf9a79
owenPatt <owen.patt.8@gmail.com>
2023-10-19 18:23:09 -0400

    Finished footer and main separation

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+//
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__dev-name">Developed by Owen Patterson</p>
+      <p className="footer__dev-date">2023</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Main/Main.js
@@ -0,0 +1,25 @@
+//
+import "./Main.css";
+import WeatherCard from "../WeatherCard/WeatherCard";
+import ItemCard from "../ItemCard/ItemCard";
+import { defaultClothingItems } from "../util/constants";
+
+function Main() {
+  const weatherTemp = "75°F";
+  return (
+    <main className="main">
+      <WeatherCard day={true} type={"cloudy"} weatherTemp={weatherTemp} />
+
+      <p className="main__temp-title">
+        Today is {weatherTemp} / You may want to wear:
+      </p>
+      <div className="main__card-container">
+        {defaultClothingItems.map((item) => {
+          return <ItemCard item={item}></ItemCard>;
+        })}
+      </div>
+    </main>
+  );
+}
+
+export default Main;
