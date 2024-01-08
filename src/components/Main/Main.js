425c1af52812be9210256abf6eef4df0e17de4b9
Owen Patterson <owen.patt.8@gmail.com>
2024-01-08 12:56:28 -0500

    Added ItemCard component to main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,6 +4,7 @@ import { useContext } from "react";
 import WeatherCard from "../WeatherCard/WeatherCard";
 import CurrentTempUnitContext from "../../contexts/CurrentTemperatureUnitContext";
 import ItemsList from "../ItemsList/ItemsList";
+import ItemCard from "../ItemCard/ItemCard";
 import getTempCategory from "../../utils/getTemperatureCategory";
 
 function Main({
